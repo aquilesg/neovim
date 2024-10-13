b29100f16bbc95a8e9dd50a66caa6cc35a678578
Aquiles Gomez <aquilesg24@gmail.com>
2024-10-13 06:14:00 -0500

    Fix codeium plugin

--- lua/configs/codeium.lua
+++ lua/configs/codeium.lua
@@ -12,3 +12,5 @@ end, { expr = true, silent = true })
 vim.keymap.set("i", "<c-x>", function()
   return vim.fn["codeium#Clear"]()
 end, { expr = true, silent = true })
+
+require("codeium").setup()
